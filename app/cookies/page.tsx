5eba8030cb5437f6dfc4294e9d17976d6562a8d6
MANYA SHUKLA <shuklamanya99@gmail.com>
2025-11-16 15:34:29 +0530

    Add /cookies route that redirects to /cookie-policy

--- app/cookies/page.tsx
+++ app/cookies/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function CookiesPage() {
-  // Permanently point /cookies to the existing cookie policy route
+
   redirect('/cookie-policy');
 }
