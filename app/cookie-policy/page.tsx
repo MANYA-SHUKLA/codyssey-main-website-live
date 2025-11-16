a954ff20f0ab2b8db4b86c4698f338a95044ef2a
MANYA SHUKLA <shuklamanya99@gmail.com>
2025-11-16 15:14:56 +0530

    Terms: update CSS import to ../components/TermsOfService.css

--- app/cookie-policy/page.tsx
+++ app/cookie-policy/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import '../../styles/CookiePolicy.css';
+import '../components/CookiePolicy.css';
 
 const CookiePolicy = () => {
   const handleAcceptAll = () => {
