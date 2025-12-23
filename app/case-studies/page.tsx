c506fa0519d4f7a0a75cb9cbb1954932d196c1eb
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2025-12-24 00:28:59 +0530

    Remove unnecessary newline in CaseStudiesPage

--- app/case-studies/page.tsx
+++ app/case-studies/page.tsx
@@ -1,7 +1,6 @@
 import CaseStudiesHero from '../components/CaseStudiesHero';
 import CaseStudiesGrid from '../components/CaseStudiesGrid';
 import CaseStudiesCTA from '../components/CaseStudiesCTA';
-
 export default function CaseStudiesPage() {
   return (
     <main className="min-h-screen bg-gray-900">
