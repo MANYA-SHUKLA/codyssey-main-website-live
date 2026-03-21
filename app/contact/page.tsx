d7476b1d278af049affe467dcfb14eef45fc28ca
MANYA SHUKLA <157141586+MANYA-SHUKLA@users.noreply.github.com>
2026-03-22 00:21:19 +0530

    Remove unnecessary blank line in Contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,6 @@
 import ContactHero from '../components/ContactHero';
 import ContactForm from '../components/ContactForm';
 import ContactInfo from '../components/ContactInfo';
-
 export default function Contact() {
   return (
     <main>
